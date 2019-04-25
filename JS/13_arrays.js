f609484570ec037f3ee3b1de759151090066d7b1
Megadeth1988 <agmkmi6@gmail.com>
2019-04-25 11:06:16 +0200

    arrays

---
+++ JS/13_arrays.js
@@ -0,0 +1,58 @@
+let numeros = [1, 2, 4, 6]
+let amigos = ['Pepe', 'Rosa', 'Juan']
+let deTodo = [1, 45, 'Perro', 'Gato', true, {nombre : 'Ramón', edad : 23}]
+
+/* console.log(numeros)
+console.log(amigos)
+console.log(deTodo) */
+
+for (let i = 0; i < amigos.length; i++) {
+    const element = amigos[i]
+    console.log(`Hola ${element}`)
+}
+
+/**
+ * 
+ * Programa que compruebe si un nombre ha sido usado alguna vez por un rey de España
+ * 
+ */
+
+/**
+ * Function isRey
+ * @name: string
+ * @returns: boolean
+ */
+
+function isRey(name) {
+    let reyes = ['Fernando', 'Isabel', 'Carlos', 'Felipe', 'Luis', 'José', 'Amadeo', 'Alfonso', 'Juan Carlos']
+    for (let i = 0; i < reyes.length; i++) {
+        const rey = reyes[i];
+        if (rey.toLowerCase() === name.toLowerCase()) {
+        return true
+    }
+}
+return false
+}
+
+/**
+ * Function mostrar
+ * @param name: string
+ * @returns: void
+ */
+
+function mostrar(name) {
+    let i = 0
+    let mensajes = [
+        `El nombre ${name} ha sido usado por un rey de España`,
+        `Ningún rey de España se ha llamado nunca ${name}`
+    ]
+    if (isRey(name)) {
+        i = 0
+    }
+    console.log(mensajes[i])
+}
+
+let name = 'Carlos'
+name = 'Ernesto'
+name = 'Felipe'
+mostrar(name)
