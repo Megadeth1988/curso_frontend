35999d8e3983ad9f9361592c6a8432910e3971e2
Megadeth1988 <agmkmi6@gmail.com>
2019-05-07 10:13:27 +0200

    Cadenas mayúsculas y minúsculas

---
+++ JS/26_mayusculas.js
@@ -0,0 +1,52 @@
+/**
+ * Dada una cadena de caracteres hay que indicar si está formada:
+ * - sólo por letras mayúsculas.
+ * - sólo por minúsculas.
+ * - por la combinación de ambas.
+ * 
+ */
+
+/**
+ * function probarCadena
+ * @param cadena: string
+ * @returns: number
+ * - 0 = sólo por letras mayúsculas
+ * - 1 = sólo por minúsculas
+ * - 2 = por la combinación de ambas
+ */
+
+ function probarCadena(cadena = '') /*Si yo no pongo nada, la variable cadena es una variable de tipo undefined*/ {
+    let r = 2 // Establecemos un valor por defecto para la variable r en la que se va a guardar el resultado, le damos un valor por defecto para que implique un tipo. El valor que ponemos por defecto coincide con el que le daríamos a la condición else en caso de que ninguno de los if se cumplan, devuelve r = 2
+     if(cadena === cadena.toUpperCase()) {
+         r = 0
+     } else if (cadena === cadena.toLowerCase()) {
+         r = 1
+     } //Aquí podríamos poner un else en el que pondríamos que si no, el valor de r sería 2, pero como lo ponemos de valor de partida, nos lo ahorramos.
+     return r
+ }
+
+ /**
+  * function comoEsLaCadena
+  * @param cadena: string
+  * @returns: void
+  * 
+  */
+ 
+  function comoEsLaCadena(cadena = '') {
+      let mensajes = [
+          'La cadena está formada sólo por letras mayúsculas',
+          'La cadena está formada sólo sólo por minúsculas',
+          'La cadena está formada por una combinación de mayúsculas y minúsculas'
+      ]
+      // let i = probarCadena(cadena)
+      // console.log(mensajes[i])
+      console.log(mensajes[probarCadena(cadena)]) // Esta es la alternativa 'pro'
+  }
+
+  let texto
+  texto = 'EN UNA GALAXIA MUY MUY LEJANA'
+  comoEsLaCadena(texto)
+  texto = 'en una galaxia muy muy lejana'
+  comoEsLaCadena(texto)
+  texto = 'En una galaxia muy muy lejana'
+  comoEsLaCadena(texto)
