42151a0cd0722d9a349c328cbe43b235c9951017
Megadeth1988 <agmkmi6@gmail.com>
2019-05-15 11:33:56 +0200

    Doble binding

---
+++ JS_Web/07_doble_binding/js/controller.js
@@ -0,0 +1,27 @@
+export function controller () {
+
+    console.log('Controller cargado')
+
+    let nombre = ''
+    let msgInicial = ''
+
+    /** Nodos del DOM */
+    const inNombre = document.querySelector('#in-nombre')
+    const output = document.querySelector('#output')
+    const btnBorrar = document.querySelector('#btn-borrar')
+
+    msgInicial = output.innerHTML + ' '
+
+    /** Asignación manejadores de eventos a los nodos */
+    inNombre.addEventListener('input', onInputNombre)
+    btnBorrar.addEventListener('click', borrar)
+    
+    function onInputNombre() {
+        nombre = inNombre.value
+        output.innerHTML = msgInicial + nombre.toUpperCase()
+    }
+
+    function borrar() {
+
+    }
+}
